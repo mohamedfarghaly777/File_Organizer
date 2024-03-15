c811f8a27c32892a1d2a9ee425b4ce048242c9a4
mohamedfarghaly777 <mo_farghaly@hotmail.com>
2024-03-15 16:11:56 +0200

    Putting code into functions

--- file_organizer.sh
+++ file_organizer.sh
@@ -1,19 +1,13 @@
-declare dir=$1
-declare tempname=""
-declare basename=""
-declare extension=""
-declare dist=""
+#!/bin/bash
 
-cd $dir
-
-# Searching in the current directory
-for file in `find $dir -maxdepth 1 -type f -ls | awk '{ print  $11 $12 }'`; do
-
-   file=$(basename "$file")
+function get_Destination()
+{
+    declare tempname=""
+    declare file=$1
+    file=$(basename "$file")
 
     if [[ ${file:0:1} == "." ]]; then
         tempname=${file:1}
-        echo "Hidden File $file"
     else
         tempname=$file
     fi
@@ -21,9 +15,30 @@ for file in `find $dir -maxdepth 1 -type f -ls | awk '{ print  $11 $12 }'`; do
     if [[ "$dist" == "$tempname" ]]; then
         dist="misc"
     fi
+    echo $dist
+}
 
+function create_dir()
+{
+    declare dist=$1
+    echo $dist
     if [[ ! -d $dist ]]; then
         mkdir $dist
     fi 
+}
+
+function main()
+{
+    # Variables
+    declare dir=$1
+    declare dist=""
+
+    cd $dir
+    # Searching in the current directory
+    for file in `find $dir -maxdepth 1 -type f -ls | awk '{ print  $11 $12 }'`; do
+    dist=`get_Destination $file`
+    create_dir $dist
     mv $file  $dist
-done
+    done
+}
+ main $1
